12219ea62e365f0209457a704e306b5360d8a8d9
sukenell <96758100+sukenell@users.noreply.github.com>
2021-12-28 12:29:02 +0900

    Update undu.groovy

--- undu.groovy
+++ undu.groovy
@@ -4,16 +4,16 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName)
     var d10 = Math.floor(ran*10)
     var d6 = Math.floor(ran*6)
     var binder = (d6+1)+(d6+1)
-    var sifter = d10+1
+    var shifter = d10+1
     var depth = 5 //이계심도
 
     if (room.indexOf("언성듀엣")!=-1){
         if (msg.indexOf("/시프트롤")!=-1){
             replier.reply(sifter)
-            if(sifter >= depth){
+            if(shifter >= depth){
                 replier.reply("성공")
             }
-            if(sifter < depth){
+            if(shifter < depth){
                 replier.reply("실패")
             }
     }
