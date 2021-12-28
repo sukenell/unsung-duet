860567433896314f1a6f66cbb34f8cc521f09930
sukenell <96758100+sukenell@users.noreply.github.com>
2021-12-28 12:26:02 +0900

    file plus

---
+++ undu.groovy
@@ -0,0 +1,40 @@
+function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName) {
+
+    var ran = Math.random()
+    var d10 = Math.floor(ran*10)
+    var d6 = Math.floor(ran*6)
+    var binder = (d6+1)+(d6+1)
+    var sifter = d10+1
+    var depth = 5 //이계심도
+
+    if (room.indexOf("언성듀엣")!=-1){
+        if (msg.indexOf("/시프트롤")!=-1){
+            replier.reply(sifter)
+            if(sifter >= depth){
+                replier.reply("성공")
+            }
+            if(sifter < depth){
+                replier.reply("실패")
+            }
+    }
+    }
+
+        if (room.indexOf("언성듀엣")!=-1){
+        if (msg.indexOf("/바인더롤")!=-1){
+            replier.reply(binder)
+            if(binder >= depth){
+                replier.reply("성공")
+            }
+            if(binder < depth){
+                replier.reply("실패")
+            }
+    }
+    }
+
+    if (room.indexOf("언성듀엣")!=-1){
+        if (msg.indexOf("/변이저항")!=-1){
+            replier.reply(d6)
+    }
+    }
+
+}
